87d9114bda728cddf2590f9fa301844920935d2b
Christofer Olaison <christofer.olaison@mentimeter.com>
2019-05-21 19:16:43 +0200

    init

---
+++ packages/config/prettier.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: "avoid",
+  jsxBracketSameLine: false,
+  bracketSpacing: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 60,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  proseWrap: "preserve",
+  insertPragma: false,
+  requirePragma: false
+};
---
+++ web/components/logotype.js
@@ -0,0 +1,14 @@
+export function Logotype({ children }) {
+  return (
+    <span
+      style={{
+        fontFamily: `"Major Mono Display", monospace`,
+        fontSize: 40,
+        letterSpacing: 1,
+        color: "#222222"
+      }}
+    >
+      {children}
+    </span>
+  );
+}
