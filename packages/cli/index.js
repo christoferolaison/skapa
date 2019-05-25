ce9dc54d50ab89d070833ae135b8b342a99675d7
Christofer Olaison <christofer.olaison@mentimeter.com>
2019-05-25 20:13:01 +0200

    cli

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,3 +1,32 @@
 #!/usr/bin/env node
+/* eslint-disable no-unused-expressions */
 
-console.log('hello skapa')
+const yargs = require('yargs')
+const execa = require('execa')
+
+const lerna = args =>
+  execa('lerna', args, { stdio: 'inherit' })
+
+const globalKeys = ['help', 'version']
+
+yargs
+  .group(globalKeys, 'Global Options:')
+  .command(
+    '$0',
+    '',
+    () => {},
+    argv => {
+      console.log('\n')
+      console.log('Welcome to skapa 🧤')
+      console.log('\n')
+    },
+  )
+  .command(
+    'release',
+    '',
+    () => {},
+    argv => {
+      lerna(['list'])
+    },
+  )
+  .help().argv
