1d756142255b4bc9388692502f576a2c32c8c074
Christofer <christofer.ps.olaison@gmail.com>
2019-08-25 17:47:45 +0200

    fix: clean

--- cli/deploy.js
+++ cli/deploy.js
@@ -12,7 +12,6 @@ exports.handler = async function({ feature }) {
   const { ZEIT_TOKEN } = process.env
   if (feature) {
     const res = now(['--token', ZEIT_TOKEN])
-    console.log(res)
   } else {
     now(['--prod', '--token', ZEIT_TOKEN])
   }
