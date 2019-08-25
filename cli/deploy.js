b8ccdc7510c2277342cdd72ffe6de4a783052625
Christofer <christofer.ps.olaison@gmail.com>
2019-08-25 17:43:22 +0200

    fix: log

--- cli/deploy.js
+++ cli/deploy.js
@@ -11,7 +11,8 @@ exports.builder = yargs => yargs.example('$0 deploy')
 exports.handler = async function({ feature }) {
   const { ZEIT_TOKEN } = process.env
   if (feature) {
-    now(['--token', ZEIT_TOKEN])
+    const res = now(['--token', ZEIT_TOKEN])
+    console.log(res)
   } else {
     now(['--prod', '--token', ZEIT_TOKEN])
   }
