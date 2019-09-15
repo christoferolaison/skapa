3649b2386d0baa85125a2d056b342e3ac567b2c3
Christofer Olaison <christofer.olaison@mentimeter.com>
2019-09-15 18:04:22 +0200

    feat: eslint-config BREAKING CHANGE: eslint config

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,66 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  parser: 'babel-eslint',
+  plugins: [
+    'react',
+    'react-hooks',
+    'flowtype',
+    'import',
+    'jsx-a11y',
+    'jest',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:flowtype/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:jsx-a11y/recommended',
+    'plugin:jest/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'flowtype/newline-after-flow-annotation': [
+      'error',
+      'always',
+    ],
+    'flowtype/sort-keys': [
+      'error',
+      'asc',
+      {
+        caseSensitive: true,
+        natural: false,
+      },
+    ],
+    'import/no-default-export': 'error',
+    'import/no-unused-modules': [
+      'error',
+      { unusedExports: true, missingExports: false },
+    ],
+    'import/order': [
+      'error',
+      { 'newlines-between': 'always' },
+    ],
+    'import/first': 'error',
+    'import/no-extraneous-dependencies': 'error',
+  },
+}
