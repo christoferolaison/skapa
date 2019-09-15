bb7a8fe2c20ae98e9bb587caabab3b7cd155573d
Christofer Olaison <christofer.olaison@mentimeter.com>
2019-09-15 16:52:37 +0200

    style: eslint

--- web/pages/index.js
+++ web/pages/index.js
@@ -2,31 +2,34 @@
 
 import React from 'react'
 import Head from 'next/head'
+
 import { Logotype } from '../components/logotype'
 
-export default () => (
-  <>
-    <Head>
-      <link
-        href="https://fonts.googleapis.com/css?family=Major+Mono+Display&display=swap"
-        rel="stylesheet"
-      />
-    </Head>
+export default function Index() {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Major+Mono+Display&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
 
-    <div
-      style={{
-        backgroundColor: '#fff',
-        margin: 0,
-        padding: 0,
-        width: '100vw',
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-      }}
-    >
-      <Logotype>Skapa Consulting</Logotype>
-    </div>
-  </>
-)
+      <div
+        style={{
+          backgroundColor: '#fff',
+          margin: 0,
+          padding: 0,
+          width: '100vw',
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'space-around',
+        }}
+      >
+        <Logotype>Skapa Consulting</Logotype>
+      </div>
+    </>
+  )
+}
