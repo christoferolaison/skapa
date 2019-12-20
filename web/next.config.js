bf647da87dcec57edfcde7a9058d5a2214604557
Christofer Olaison <christofer.ps.olaison@gmail.com>
2019-12-20 22:39:02 +0100

    chore: add packtracker to web (#18)

--- web/next.config.js
+++ web/next.config.js
@@ -1,5 +1,19 @@
+const {
+  StatsWriterPlugin,
+} = require('webpack-stats-plugin')
 const withMDX = require('@next/mdx')()
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'mdx'],
+  webpack: config => {
+    config.plugins.push(
+      new StatsWriterPlugin({
+        stats: {
+          source: false,
+          excludeAssets: false,
+        },
+      }),
+    )
+    return config
+  },
 })
