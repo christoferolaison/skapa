74b2ce44612cd7d0d1c9eed0b6c08c2591cc406b
Christofer <christofer.ps.olaison@gmail.com>
2019-12-20 16:00:11 +0100

    dfg

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -9,6 +9,7 @@ export default function Index() {
   return (
     <>
       <Heading>Blog</Heading>
+      <Heading>Blog</Heading>
       <Link href="/create-a-blog-post-using-next-mdx-and-prism">
         Create a blog post using Next.js, MDX and Prism
       </Link>
