cd5c725ee972bf11825801c9242887d79f3edd1e
Christofer <christofer.ps.olaison@gmail.com>
2019-12-20 23:57:06 +0100

    chore: add lodash

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,14 +1,14 @@
 // @flow
 
 import React from 'react'
+import _ from 'lodash'
 
 import { Heading } from '../components/heading'
 import { Link } from '../components/link'
-
+console.log(_)
 export default function Index() {
   return (
     <>
-      <Heading>Blog</Heading>
       <Heading>Blog</Heading>
       <Link href="/create-a-blog-post-using-next-mdx-and-prism">
         Create a blog post using Next.js, MDX and Prism
